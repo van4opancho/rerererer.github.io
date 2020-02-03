94461e1f5ca30f0b37aa0e9dbc946d4c966a378e
van4opancho <58988841+van4opancho@users.noreply.github.com>
2020-02-03 20:07:19 +0200

    Add files via upload

---
+++ success.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html><html lang="en"><head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Grazie mille!</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap&subset=cyrillic" rel="stylesheet">
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+  <main class="main">
+    <div class="main-heading">
+      <h1>Grazie mille!</h1>
+      <span>Il tuo ordine è stato accettato</span>
+    </div>
+    <div class="main-trigger">
+      <span>solo OGGI <br>
+        c'è un'offerta per te</span>
+    </div>
+    <div class="main-present">
+      <div class="img-wrap">
+        <img src="assets/present.png" alt>
+      </div>
+      <span class="main-present__top red">un regalo!</span>
+      <p class="main-present__price"><span class="red">88€</span>consegna gratuita</p>
+    </div>
+    <p class="main-present-after">Se compri <span class="red">2</span> <span class="product-color">INVINCEABLE CLEANER</span> ottieni uno sconto, <br> <span class="red">il terzo</span> <span class="product-color">INVINCEABLE CLEANER</span> di regalo e la consegna gratuita!</p>
+
+    <div class="main-builets">
+      <ul>
+        <li>Ordina <span class="red">2</span> <span class="product-color">INVINCEABLE CLEANER</span></li>
+        <li>Paga <span class="red">88 euro per due</span>, invece di 98 euro</li>
+        <li>Ottieni un'altro <span class="product-color">INVINCEABLE CLEANER</span> di regalo</li>
+        <li>Il tuo ordine sarà consegnato <br> <span class="red">GRATIS</span></li>
+      </ul>
+      <div class="main-builets__bot">risparmi 58 euro</div>
+    </div>
+  </main>
+
+
+<!-- Facebook Pixel Code -->
+<script>
+ !function(f,b,e,v,n,t,s)
+ {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+ n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+ if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+ n.queue=[];t=b.createElement(e);t.async=!0;
+ t.src=v;s=b.getElementsByTagName(e)[0];
+ s.parentNode.insertBefore(t,s)}(window, document,'script',
+ 'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '537857156816671');
+ fbq('track', 'PageView');
+fbq('track', 'Lead');
+</script>
+<noscript><img src="https://www.facebook.com/tr?id=537857156816671&ev=Lead&noscript=1" /></noscript>
+<!-- End Facebook Pixel Code -->
+</body></html>
